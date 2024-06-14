cfd204dc6854fad734370069a5acffbe678526e7
amird55 <amir.dgn@outlook.com>
2024-06-14 13:24:58 +0300

    שליטה על הדלקת וכיבוי נורה

--- Catch_OneLed.ino
+++ Catch_OneLed.ino
@@ -1,9 +1,38 @@
-void setup() {
-  // put your setup code here, to run once:
+#define pinLed 6
+bool IsLedOn;
+unsigned long LastToggleTime;
 
+void setup() {
+  Serial.begin(9600);
+  randomSeed(analogRead(A1));
+
+  pinMode(pinLed,OUTPUT);
+  LedOff();
+  LastToggleTime=millis();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int BlinkInterval=random(1000,3001);
 
+  if(millis()-LastToggleTime > BlinkInterval){
+    LedToggle();
+    LastToggleTime=millis();
+  }
+
+}
+
+void LedToggle(){
+  if(IsLedOn){
+    LedOff();
+  } else {
+    LedOn();
+  }
+}
+void LedOn(){
+  digitalWrite(pinLed,HIGH);
+  IsLedOn=true;
+}
+void LedOff(){
+  digitalWrite(pinLed,LOW);
+  IsLedOn=false;
 }
