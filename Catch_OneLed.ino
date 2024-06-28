d7f833666913b8e67e7ecc0a20b8b3507d5f0b64
amird55 <amir.dgn@outlook.com>
2024-06-28 11:52:40 +0300

    זיהוי לחיצה על כפתור והעלאת תוצאה לפי זה

--- Catch_OneLed.ino
+++ Catch_OneLed.ino
@@ -1,11 +1,21 @@
 #define pinLed 6
+#define pinBtn 2
+
 bool IsLedOn;
 unsigned long LastToggleTime;
 int BlinkInterval;
 
+int CurrBtn;
+int LastBtn;
+unsigned long LastPressTime;
+int score=0;
+
 void setup() {
   Serial.begin(9600);
   randomSeed(analogRead(A1));
+
+  pinMode(pinBtn,INPUT_PULLUP);
+  LastPressTime=millis();
 
   pinMode(pinLed,OUTPUT);
   LedOff();
@@ -20,6 +30,14 @@ void loop() {
     LedToggle();
     LastToggleTime=millis();
   }
+
+  CurrBtn=digitalRead(BTN_pin);
+  if((CurrBtn == LOW) && (LastBtn == HIGH)&&(millis() - LastPressTime > 50)){
+    LastPressTime=millis();
+    // btn is pressed
+    score += (IsLedOn) ? 1 : -1 ;
+  }
+  LastBtn=CurrBtn;
 
 }
 
