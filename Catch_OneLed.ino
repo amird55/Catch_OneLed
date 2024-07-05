58dfd86563d2153f668c4de117f91a803fde0cd4
amird55 <amir.dgn@outlook.com>
2024-07-05 12:33:48 +0300

    מכונת מצבים לניהול המשחק

--- Catch_OneLed.ino
+++ Catch_OneLed.ino
@@ -3,6 +3,12 @@
 
 #define pinLedYellow 4
 
+#define WAIT_FOR_NEW_GAME 31
+#define START_OF_GAME     32
+#define GAME_IS_ON        33
+#define GAME_OVER         34
+int CurrState;
+
 bool IsLedOn;
 unsigned long LastToggleTime;
 int BlinkInterval;
@@ -22,19 +28,47 @@ void setup() {
   LastPressTime=millis();
 
   pinMode(pinLed,OUTPUT);
-  LedOff();
-  LastToggleTime=millis();
-  BlinkInterval=random(200,3001);
-
   pinMode(pinLedYellow,OUTPUT);
 
+  CurrState=WAIT_FOR_NEW_GAME;
 
-  MaxBlinks=map(analogRead(A1),0,1023,7,12);
-  
 }
 
 void loop() {
-  if(blinkCnt < MaxBlinks){
+  switch(CurrState){
+    case WAIT_FOR_NEW_GAME:wait_to_start(); break;
+    case START_OF_GAME    :start_game();    break;
+    case GAME_IS_ON       :game_on();       break;
+    case GAME_OVER        :game_over();     break;
+
+  }
+}
+      
+void wait_to_start(){
+  digitalWrite(pinLed,HIGH);
+  digitalWrite(pinLedYellow,HIGH);
+  //זיהוי שחרור של הכפתור
+    CurrBtn=digitalRead(pinBtn);
+    if((CurrBtn == HIGH) && (LastBtn == LOW)&&(millis() - LastPressTime > 50)){
+      LastPressTime=millis();
+      // btn is released
+      CurrState=START_OF_GAME;
+    }
+    LastBtn=CurrBtn;
+
+}
+void start_game(){
+  score=0;
+  blinkCnt=0;
+  LedOff();
+  LastToggleTime=millis();
+  BlinkInterval=random(200,3001);
+  MaxBlinks=map(analogRead(A0),0,1023,7,12);
+
+  CurrState=GAME_IS_ON;
+}
+void game_on(){
+  if(blinkCnt <= MaxBlinks){
     if(millis()-LastToggleTime > BlinkInterval){
       BlinkInterval=random(200,3001);
       LedToggle();
@@ -58,8 +92,15 @@ void loop() {
     int ledValY = map(score,0,5,0,128);
     analogWrite(pinLedYellow,ledValY);
   }
+  else {
+    CurrState=GAME_OVER;
+  }
 }
-
+void game_over(){
+     int ledValY = map(score,0,5,0,128);
+    analogWrite(pinLedYellow,ledValY);
+    CurrState=WAIT_FOR_NEW_GAME;
+}
 void LedToggle(){
   if(IsLedOn){
     LedOff();
