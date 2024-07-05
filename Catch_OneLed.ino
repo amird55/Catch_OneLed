e4050e1a7c86a5315c8ec86f82818fa18d17a8a4
amird55 <amir.dgn@outlook.com>
2024-07-05 11:48:09 +0300

    הגבלת כמות הבהובים לפי פוטנציומטר

--- Catch_OneLed.ino
+++ Catch_OneLed.ino
@@ -12,6 +12,8 @@ int LastBtn;
 unsigned long LastPressTime;
 int score=0;
 
+int MaxBlinks=5;
+int blinkCnt=0;
 void setup() {
   Serial.begin(9600);
   randomSeed(analogRead(A1));
@@ -26,29 +28,36 @@ void setup() {
 
   pinMode(pinLedYellow,OUTPUT);
 
+
+  MaxBlinks=map(analogRead(A1),0,1023,7,12);
+  
 }
 
 void loop() {
+  if(blinkCnt < MaxBlinks){
+    if(millis()-LastToggleTime > BlinkInterval){
+      BlinkInterval=random(200,3001);
+      LedToggle();
+      LastToggleTime=millis();
+      if(IsLedOn){
+        blinkCnt++;
+      }
+    }
 
-  if(millis()-LastToggleTime > BlinkInterval){
-    BlinkInterval=random(200,3001);
-    LedToggle();
-    LastToggleTime=millis();
+    CurrBtn=digitalRead(pinBtn);
+    if((CurrBtn == LOW) && (LastBtn == HIGH)&&(millis() - LastPressTime > 50)){
+      LastPressTime=millis();
+      // btn is pressed
+      score += (IsLedOn) ? 1 : -1 ;
+      score = max(0,score);
+      score = min(5,score);
+    }
+    LastBtn=CurrBtn;
+
+
+    int ledValY = map(score,0,5,0,128);
+    analogWrite(pinLedYellow,ledValY);
   }
-
-  CurrBtn=digitalRead(pinBtn);
-  if((CurrBtn == LOW) && (LastBtn == HIGH)&&(millis() - LastPressTime > 50)){
-    LastPressTime=millis();
-    // btn is pressed
-    score += (IsLedOn) ? 1 : -1 ;
-    score = max(0,score);
-    score = min(5,score);
-  }
-  LastBtn=CurrBtn;
-
-
-  int ledValY = map(score,0,5,0,128);
-  analogWrite(pinLedYellow,ledValY);
 }
 
 void LedToggle(){
